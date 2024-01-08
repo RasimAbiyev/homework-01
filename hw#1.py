cbf32da931b2925048f3b7bc1e3ed0d4cd159ff8
RasimAbiyev <abiyev.rasim@gmail.com>
2024-01-08 13:19:34 +0400

    first commit

---
+++ hw#1.py
@@ -0,0 +1,50 @@
+# """
+# - Printing -
+# A. Write a simple program that prints "Python is the beast!"
+print("Python is the beast!")
+
+# B. Write a q1simple program that prints "I love coding."
+print("I love coding.")
+
+# C. Write a simple program that prints "I am going to master Python soon"
+print("I am going to master Python soon")
+
+# D. Write a simple program that prints "I am learning Python, hooray!!"
+print("I am learning Python, hooray!!")
+
+# E. Write a simple program that prints "Python is the most powerful programming language!"
+print("Python is the most powerful programming language!")
+
+# - Variables -
+# A. Create a variable named "fruit" and assign any fruit-related string to it. Print that variable.
+fruit = "Apple"
+print(fruit)
+
+# B. Create a variable named "sentence" and assign any string to it. Print that variable.
+sentence = "My name is Rasim"
+print(sentence)
+
+# C. Create a variable named "name" and assign any name-related string to it. Print that variable.
+name = "Rasim"
+print(name)
+
+# D. Create a variable named "surname" and assign any name-related string to it. Print that variable.
+surname = "Abiyev"
+print(surname)
+
+# E. Create a variable named "full_name" and assign any name-related string to it. Print that variable.
+full_name = "Rasim Abiyev"
+print(full_name)
+
+# F. Create a variable named "colour" and assign any colour-related string to it. Print that variable.
+colour = "Blue"
+print(colour)
+
+# G. Create a variable named "gender" and assign any gender-related string to it. Print that variable.
+gender = "Male"
+print(gender)
+
+# H. Create a variable named "brand" and assign any brand-related string to it. Print that variable.
+brand = "Nike"
+print(brand)
+# """
